cb0a597589714a7c0dfd827bceb3587082ddc52c
omni <gaurijaiswal345@gmail.com>
2026-03-13 21:30:49 +0530

    updated initial

---
+++ new.py
@@ -0,0 +1,34 @@
+# Program to store employee details and generate salary
+
+# Taking employee information
+name = input("Enter Employee Name: ")
+emp_id = input("Enter Employee ID: ")
+department = input("Enter Department: ")
+basic_salary = float(input("Enter Basic Salary: "))
+
+# Calculating allowances
+DA = 0.92 * basic_salary
+HRA = 0.58 * basic_salary
+TA = 0.30 * basic_salary
+
+# LIC deduction
+LIC = 500
+
+# Gross and Net Salary
+gross_salary = basic_salary + DA + HRA + TA
+net_salary = gross_salary - LIC
+
+# Displaying employee details and salary
+print("\nEmployee Details")
+print("Name:", name)
+print("Employee ID:", emp_id)
+print("Department:", department)
+
+print("\nSalary Details")
+print("Basic Salary:", basic_salary)
+print("DA:", DA)
+print("HRA:", HRA)
+print("TA:", TA)
+print("LIC Deduction:", LIC)
+print("Gross Salary:", gross_salary)
+print("Net Salary:", net_salary)
